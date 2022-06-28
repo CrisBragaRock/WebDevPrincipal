9bad2191154f2b42d16a21e308758831b4d82d2a
Cristian Talys Braga Borges <88721660+CrisBragaRock@users.noreply.github.com>
2022-06-28 08:36:44 -0300

    aula 153 completa

---
+++ 004-estudos/estudos-JS/aula153/JS/script.js
@@ -0,0 +1,16 @@
+//string
+var texto = "Curso de JavaScript"
+
+//number
+var numeroInteiro = -7
+var numeroFracionado = 10.15
+
+//boolean
+var teste = false
+var teste2 = true
+
+console.log(texto)
+console.log(numeroInteiro)
+console.log(numeroFracionado)
+console.log(teste)
+console.log(teste2)
