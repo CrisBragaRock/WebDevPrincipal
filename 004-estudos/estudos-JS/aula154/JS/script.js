71b2293ce0cde6914ac88d62723480ddae41a677
Cristian Talys Braga Borges <88721660+CrisBragaRock@users.noreply.github.com>
2022-06-28 08:55:32 -0300

    Aula 154

---
+++ 004-estudos/estudos-JS/aula154/JS/script.js
@@ -0,0 +1,8 @@
+// var nome = "Cristian"
+// var idade = "25"
+
+var nome = prompt('Digite o seu nome:')
+var idade = prompt('Digite a sua idade:')
+//O prompt sempre receberá os valores como sendo string.
+
+document.write(`<h2>Olá ${nome}, tudo bem? Estamos vendo aqui que você possui ${idade} anos.</h2>`)
