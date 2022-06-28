b6266ddb2bebe57dc0ae39636d81070d6b14d02e
Cristian Talys Braga Borges <88721660+CrisBragaRock@users.noreply.github.com>
2022-06-28 15:34:39 -0300

    aula 156

---
+++ 004-estudos/estudos-JS/aula156/JS/script.js
@@ -0,0 +1,15 @@
+/*
+    Ambos são considerados valores primitivos. A diferença entre os dois é que
+    null representa a ausencia intencional de um valor.
+*/
+
+//null
+var teste1 = null
+
+//undefined
+var teste2 = undefined 
+var teste3 //podemos não declarar o tipo da variavel e ela será undefined
+
+console.log(teste1)
+console.log(teste2)
+console.log(teste3)
