4954be5072afdbbea8925f60226f56957d9f7bd9
Cristian Talys Braga Borges <88721660+CrisBragaRock@users.noreply.github.com>
2022-06-28 15:58:54 -0300

    aula 157

---
+++ 004-estudos/estudos-JS/aula157/JS/script.js
@@ -0,0 +1,17 @@
+/*
+    Varáveis podem e normalmente são alterados durante o precessamento dos nossos script.
+    Pra atender determinadas lógicas, naturalmente, é necessário modificar os valores das
+    variáveis em certos momentos.
+*/
+
+var teste1 = null
+
+console.log(teste1)
+
+teste1 = 'Valor modificado com sucesso!'
+
+console.log(teste1)
+
+teste1 = 147.65
+
+console.log(teste1)
