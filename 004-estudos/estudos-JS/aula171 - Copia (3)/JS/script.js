68747fd3fd03837e6bcce3a24f089a3751b686fe
Cristian Talys Braga Borges <88721660+CrisBragaRock@users.noreply.github.com>
2022-07-01 02:21:29 -0300

    Adição de pastas para ajuste

---
+++ 004-estudos/estudos-JS/aula171 - Copia (3)/JS/script.js
@@ -0,0 +1,18 @@
+var num1 = 9
+var num2 = 2
+
+
+//soma
+document.write('A soma entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 + num2) + '<br>')
+//subtração
+document.write('A subtração entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 - num2) + '<br>')
+// multiplicação
+document.write('A multiplicação entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 * num2) + '<br>')
+// divisão
+document.write('A divisão entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 / num2) + '<br>')
+// módulo
+document.write('O módulo entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 % num2) + '<br>')
+//incremento
+document.write('O incremento de ' + num1 + ' é: ' + (++num1) + '<br>')
+//decremento
+document.write('A subtração entre ' + num2 + ' é: ' + (--num2) + '<br>')
---
+++ 004-estudos/estudos-JS/aula173 - Copia (3)/JS/script.js
@@ -0,0 +1,3 @@
+document.write(11 + 5 - 5 * 2 / 2 + '<br>')
+
+document.write((11 + 5 - 5 * 2) / 2)
