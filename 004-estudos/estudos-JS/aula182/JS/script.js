b2eb1aaae32c01ab5def866699157b46387ad7b6
Cristian Talys Braga Borges <88721660+CrisBragaRock@users.noreply.github.com>
2022-07-04 02:26:48 -0300

    aula 182 ok

--- 004-estudos/estudos-JS/aula182/JS/script.js
+++ 004-estudos/estudos-JS/aula182/JS/script.js
@@ -1,18 +1,15 @@
-var num1 = 9
-var num2 = 2
+//método Math.ceil()
+var a = Math.ceil(9.380)
+document.write(a + '<br>')
 
+//método floor
+var b = Math.floor(10.380)
+document.write(b + '<br>')
 
-//soma
-document.write('A soma entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 + num2) + '<br>')
-//subtração
-document.write('A subtração entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 - num2) + '<br>')
-// multiplicação
-document.write('A multiplicação entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 * num2) + '<br>')
-// divisão
-document.write('A divisão entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 / num2) + '<br>')
-// módulo
-document.write('O módulo entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 % num2) + '<br>')
-//incremento
-document.write('O incremento de ' + num1 + ' é: ' + (++num1) + '<br>')
-//decremento
-document.write('A subtração entre ' + num2 + ' é: ' + (--num2) + '<br>')
+//método round
+var c = Math.round(9.5)
+document.write(c + '<br>')
+
+//método random
+var d = Math.random()
+document.write(d + '<br>')
