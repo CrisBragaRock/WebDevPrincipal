c99418857209a747a07a43ac0b3cc82d2e944a05
Cristian Talys Braga Borges <88721660+CrisBragaRock@users.noreply.github.com>
2022-07-06 16:40:57 -0300

    uaulas até 190 ok

--- 004-estudos/estudos-JS/aula188/JS/script.js
+++ 004-estudos/estudos-JS/aula188/JS/script.js
@@ -1,24 +1,11 @@
-//soma
-var teste = 10
-teste += 5
-console.log(teste)
+function acao1() {
+    alert('Tecla pressinada!')
+}
 
-//subtração
-var teste1 = 10
-teste1 -= 5
-console.log(teste1)
+function acao2() {
+    alert('Tecla liberada!')
+}
 
-//multiplicação
-var teste2 = 10
-teste2 *= 5
-console.log(teste2)
-
-//divisão
-var teste3 = 10
-teste3 /= 5
-console.log(teste3)
-
-//módulo
-var teste4 = 10
-teste4 %= 3.5
-console.log(teste4)
+function acao3() {
+    alert('Tecla sendo pressionada continuamente!')
+}
--- 004-estudos/estudos-JS/aula189/JS/script.js
+++ 004-estudos/estudos-JS/aula189/JS/script.js
@@ -1,24 +1,9 @@
-//soma
-var teste = 10
-teste += 5
-console.log(teste)
+function resize() {
+    document.getElementById('size').innerHTML = '<h1>Tamanho da janela alterado!</h1>'
+    document.getElementById('size').style.backgroundColor = 'lightgray'
+}
 
-//subtração
-var teste1 = 10
-teste1 -= 5
-console.log(teste1)
-
-//multiplicação
-var teste2 = 10
-teste2 *= 5
-console.log(teste2)
-
-//divisão
-var teste3 = 10
-teste3 /= 5
-console.log(teste3)
-
-//módulo
-var teste4 = 10
-teste4 %= 3.5
-console.log(teste4)
+function rolar() {
+    document.getElementById('scroll').style.backgroundColor = 'lightgreen'
+    document.getElementById('scroll').innerHTML = '<h1>Rolagem acionada!</h1>'
+}
