df2483e5c595e570e77874ec90101b60b8de049b
Cristian Talys Braga Borges <88721660+CrisBragaRock@users.noreply.github.com>
2022-07-07 01:41:18 -0300

    aula 193 ok

--- 004-estudos/estudos-JS/aula193/JS/script.js
+++ 004-estudos/estudos-JS/aula193/JS/script.js
@@ -5,6 +5,26 @@ function verificarCaracter() {
     //limpar o campo de digitação
     document.getElementById('entrada').value = ''
 
-    console.log(caracter)
+    //limpar espaços em branco
+    caracter.trim()
+
+    switch(caracter) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            document.getElementById('apenasNumeros').value += caracter
+            break;
+            default:
+                document.getElementById('apenasLetras').value += caracter
+    }
 }
+
+//limpar espaçoes em branco
 
