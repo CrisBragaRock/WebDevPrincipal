1ea0d77a3bca6f084a5b8fafaa69e5d434c5a3f1
Cristian Talys Braga Borges <88721660+CrisBragaRock@users.noreply.github.com>
2022-07-13 01:14:29 -0300

    aula 204 ok

---
+++ 004-estudos/estudos-JS/aula202/JS/script.js
@@ -0,0 +1,20 @@
+var lista_frutas = Array()
+
+lista_frutas[0] = 'Banana'
+lista_frutas[1] = 'Maçã'
+
+console.log(lista_frutas)
+
+//incluir elementos no final do array
+lista_frutas.push('Uva')
+//incluir elementos no inicio do array
+lista_frutas.unshift('Amora')
+
+console.log(lista_frutas)
+
+//excluir elementos do final do array
+lista_frutas.pop()
+//excluir elementos do inicio do array
+lista_frutas.shift()
+
+console.log(lista_frutas)
